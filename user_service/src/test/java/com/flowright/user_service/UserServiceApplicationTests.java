d9e5e69acd498746d60ead190828c1c4080a2187
KhanhRomVN <87371837+KhanhRomVN@users.noreply.github.com>
2024-10-31 21:42:50 +0700

    update

---
+++ user_service/src/test/java/com/flowright/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.flowright.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
