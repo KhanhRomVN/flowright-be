00908f59c17f588fe40cdfe070d7cb7f71902520
KhanhRomVN <87371837+KhanhRomVN@users.noreply.github.com>
2024-10-31 23:46:36 +0700

    update api-gateway

--- api_gateway/src/main/java/com/flowright/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/flowright/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.flowright.gateway;
+package com.flowright.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
